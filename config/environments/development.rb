db3aa34857837609b87adbe0f8f587467a7c4143
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-05-18 00:23:47 -0400

    Get ActiveStorage service names from env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-  # config.active_storage.service = :amazon_staging
+  config.active_storage.service = (ENV['ACTIVE_STORAGE_SERVICE'] || 'local').to_sym
+  # config.active_storage.service = :local
   # config.active_storage.service = :mirror_local_to_amazon_staging
 
   # Don't care if the mailer can't send.
