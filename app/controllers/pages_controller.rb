b0af438aa215e5c36e8b5af21f6d67baf0146f3d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-04-19 21:50:58 -0400

    Change caching strategy
    Oops. Previous attempt was only caching the results of queries coming
    through the graphiql web ui.
    
    This commit reverts that, and instead applies a simpler layer of
    caching to the pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,18 @@
 class PagesController < ApplicationController
   def projects
     query = read_query 'projects'
-    @props = Portfolio18Schema.execute query
+    @props = cached_result query
   end
 
   private
 
   def read_query(filename)
     File.read(Rails.root.join('app', 'queries', "#{filename}.graphql"))
+  end
+
+  def cached_result(query)
+    Rails.cache.fetch(Digest::MD5.hexdigest(query), expires_in: Rails.application.config_for(:cache)['ttl']) do
+      Portfolio18Schema.execute(query).to_h
+    end
   end
 end
